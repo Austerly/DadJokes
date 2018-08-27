4cd675c11defe725ea12ec93b14d34940218de3a
Benjamin Poulsen PRO <benjaminpoulsenpro@Benjamins-MacBook-Air.local>
2018-08-27 09:43:17 -0600

    first dad joke

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("can you put the cat out?")
+print("I didn't know the cat was on fire")
+
